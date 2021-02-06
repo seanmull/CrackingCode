33e4df051844d7bdaa2f81888f91480ba8bf564f
SeansPC <SeansPC@DESKTOP-JQ7P2UF>
2021-02-05 22:11:24 -0800

    Branch2

--- Prime/src/prime/Prime.java
+++ Prime/src/prime/Prime.java
@@ -5,15 +5,11 @@ public class Prime {
 
 	public static void main(String[] args) {
 		//Enter a number and print a message if it is prime or not
-		//Test if push worked
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter a number:  ");
 		int myNumber = scanner.nextInt();
-		
-		// Brute force implementation
-		/*int multiple = 2;
+		int multiple = 2;
 		while(multiple != myNumber) {
-			
 			if(myNumber%multiple == 0) {
 				System.out.println("Number is not prime");
 				System.exit(0);
@@ -21,13 +17,6 @@ public class Prime {
 			multiple++;
 		}
 		System.out.println("Number is a prime");
-		scanner.close();*/
-		
-		// More optimized implementation
-		// The issue is other multiples are not skipped in this check
-		// For instance when we check for 2 we know 4,6,8.. are not multiples too
-		
-	
+		scanner.close();
 	}
-
 }
