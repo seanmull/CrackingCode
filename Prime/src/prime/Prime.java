4faf23f5a04790301227e684fb70e6e5a53c9cfb
seanmull <kimlan065@gmail.com>
2021-02-05 16:34:40 -0800

    TODO find more optimal solution to algorithm

--- Prime/src/prime/Prime.java
+++ Prime/src/prime/Prime.java
@@ -4,21 +4,13 @@ import java.util.Scanner;
 public class Prime {
 
 	public static void main(String[] args) {
-		//enter number
-		
-		//commit 2
-		//loop through the possible multiples of number
-		//compare multiple with number
-		//stop if it is a multiple, print not prime
-		//continue with loop
-		
-		//print prime
+		//Enter a number and print a message if it is prime or not
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter a number:  ");
 		int myNumber = scanner.nextInt();
-		//System.out.print(myNumber);
 		
-		int multiple = 2;
+		// Brute force implementation
+		/*int multiple = 2;
 		while(multiple != myNumber) {
 			
 			if(myNumber%multiple == 0) {
@@ -28,7 +20,13 @@ public class Prime {
 			multiple++;
 		}
 		System.out.println("Number is a prime");
-		scanner.close();
+		scanner.close();*/
+		
+		// More optimized implementation
+		// The issue is other multiples are not skipped in this check
+		// For instance when we check for 2 we know 4,6,8.. are not multiples too
+		
+	
 	}
 
 }
