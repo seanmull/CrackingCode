377aecbac59590b296a8a6fcda2ecd204bca757a
SeansPC <SeansPC@DESKTOP-JQ7P2UF>
2021-02-06 21:01:53 -0800

    second commit

--- CrackingTheCode/src/crackingthecode/CheckPerm.java
+++ CrackingTheCode/src/crackingthecode/CheckPerm.java
@@ -5,7 +5,7 @@ public class CheckPerm {
 	public static void main(String[] args) {
 		/*Check Permutation: Given two strings, write a method to decide if one is a permutation of the
 		other. */ 
-		String s;
+		
 	}
 
 }
