c793ddb046d335f210f23e143e89f2ca7089c854
SeansPC <SeansPC@DESKTOP-JQ7P2UF>
2021-02-06 20:46:38 -0800

    Pushing strings and arrays problems

---
+++ CrackingTheCode/src/crackingthecode/CheckPerm.java
@@ -0,0 +1,10 @@
+package crackingthecode;
+
+public class CheckPerm {
+
+	public static void main(String[] args) {
+		/*Check Permutation: Given two strings, write a method to decide if one is a permutation of the
+		other. */
+	}
+
+}
---
+++ CrackingTheCode/src/crackingthecode/ZeroMatrix.java
@@ -0,0 +1,10 @@
+package crackingthecode;
+
+public class ZeroMatrix {
+
+	public static void main(String[] args) {
+		/*Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
+		column are set to 0. */
+	}
+
+}
