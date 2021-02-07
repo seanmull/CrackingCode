d495631c43ccc7c474eeb8aa87807f467b43e8e3
SeansPC <SeansPC@DESKTOP-JQ7P2UF>
2021-02-06 17:02:20 -0800

    first commit

---
+++ CrackingTheCode/src/crackingthecode/IsUnique.java
@@ -0,0 +1,11 @@
+package crackingthecode;
+
+public class IsUnique {
+
+	public static void main(String[] args) {
+		/*Implement an algorithm to determine if a string has all unique
+		 characters.What if you cannot uses additional data structures*/
+		
+	}
+
+}
