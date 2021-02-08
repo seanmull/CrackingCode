e47e91d0f133daf907de0fd2eba922d9e0770984
seanmull <kimlan065@gmail.com>
2021-02-08 12:47:01 -0800

    Changed to linkedlist from arraylist

--- CrackingTheCode/src/crackingthecode/CheckPerm.java
+++ CrackingTheCode/src/crackingthecode/CheckPerm.java
@@ -1,19 +1,19 @@
 package crackingthecode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class CheckPerm {
 
 	public static void main(String[] args) {
 		/*Check Permutation: Given two strings, write a method to decide if one is a permutation of the
 		other. */ 
-		String s1 = "dogasdadd";
-		String s2 = "gfdasdada";
+		String s1 = "sag";
+		String s2 = "gas";
 		
 		//Brute force algorthm
-		ArrayList<String> String1 = new ArrayList<String>(Arrays.asList(s1.split("")));
-		ArrayList<String> String2 = new ArrayList<String>(Arrays.asList(s2.split("")));
+		LinkedList<String> String1 = new LinkedList<String>(Arrays.asList(s1.split("")));
+		LinkedList<String> String2 = new LinkedList<String>(Arrays.asList(s2.split("")));
 
 		boolean flag; //flag to see if char match
 		while(!String1.isEmpty()) {
@@ -33,7 +33,7 @@ public class CheckPerm {
 		}
 		System.out.println(s2 + " is a permutation of " + s1);
 	
-		//Runtime is 2N for the string to Arraylist conversion,
+		//Runtime is 2N for the string to Linkedlist conversion,
 		//We then do N + N-1 + N-2 ... 1 comparisions
 		//Runtime is big O of N^2 the only better solution would be to sort the array
 		//then compare, but then the problem would be trivial.
