5e8d2d7660ae15a9502a976d0925d6f13fbdc087
SeansPC <SeansPC@DESKTOP-JQ7P2UF>
2021-02-08 21:31:35 -0800

    Added OneAway

--- CrackingTheCode/src/crackingthecode/OneAway.java
+++ CrackingTheCode/src/crackingthecode/OneAway.java
@@ -1,5 +1,8 @@
 package crackingthecode;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class OneAway {
 
 	public static void main(String[] args) {
@@ -11,8 +14,39 @@ public class OneAway {
 		pales, pale -> true
 		pale, bale -> true
 		pale, bake -> false*/
-		//s
-		//Check
+		String s1 = "brag";
+		String s2 = "bra";
+		
+		LinkedList<String> string1 = new LinkedList<String>(Arrays.asList(s1.split("")));
+		LinkedList<String> string2 = new LinkedList<String>(Arrays.asList(s2.split("")));
+		
+		int diff = s1.length() - s2.length();
+		if(Math.abs(diff) > 1) {
+			System.out.println(s1 + " is not one away from " + s2);
+			System.exit(0);
+		}else if(diff > 0) {
+			string2.addLast(" ");
+		}else if(diff < 0){
+			string1.addLast(" ");
+		}
+		
+		int counter = 0;
+		for(int i = 0; i < string1.size(); i++) {
+			if(!string1.get(i).equals(string2.get(i))) {
+				counter++;
+			}
+		}
+		if(counter == 1) {
+			System.out.println(s1 + " is one away from " + s2);
+		}else {
+			System.out.println(s1 + " is not one away from " + s2);
+		}
+		
+		//Each linked list creations is N so 2N runtime
+		//The adding of elements is constant 1 runtime
+		//Checking each element N runtime so big 0(N) runtime.
+		//Sorting wouldn't helps since quick sort its runtime is NlogN
+			
 
 
 	}
