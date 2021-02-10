bd8f1dfc2d1ad3cedaaf8815d295656ce78f4f0f
seanmull <kimlan065@gmail.com>
2021-02-09 21:29:26 -0800

    Started StringRotation

--- CrackingTheCode/src/crackingthecode/StringRotation.java
+++ CrackingTheCode/src/crackingthecode/StringRotation.java
@@ -1,11 +1,32 @@
 package crackingthecode;
 
+import java.util.ArrayList;
+
 public class StringRotation {
 
 	public static void main(String[] args) {
 		/*String Rotation:Assumeyou have a method isSubstringwhich checks if oneword is a substring
 		of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one
 		call to isSubstring (e.g., "waterbottle" is a rotation of"erbottlewat"). *///
+		String string = "waterbottle";
+		String stringRotate = "erbottlewat";
+		ArrayList<Integer> indexList = new ArrayList();
+		ArrayList<String> stringRotateList = new ArrayList();
+		char firstChar = stringRotate.charAt(0);
+		
+		for (int i = 0; i < string.length(); i++){
+			if(string.charAt(i) == firstChar) {
+				indexList.add(i);
+			}
+		}
+		
+		//TODO
+		//Create rotated strings from index found in locations
+			//Break string into two substrings
+			//Join them in reverse order
+		//Compare those strings with stringRotate
+		
+				
 	}
 
 }
