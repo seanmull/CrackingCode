b4b51305e6e3252a98bdd7be4a705a9b04dc5aef
seanmull <kimlan065@gmail.com>
2021-02-09 16:50:03 -0800

    Started string compression

--- CrackingTheCode/src/crackingthecode/StringCompression.java
+++ CrackingTheCode/src/crackingthecode/StringCompression.java
@@ -1,5 +1,8 @@
 package crackingthecode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StringCompression {
 
 	public static void main(String[] args) {
@@ -7,7 +10,19 @@ public class StringCompression {
 		of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
 		"compressed" string would not become smaller than the original string, your method should return
 		the original string. You can assume the string has only uppercase and lowercase letters (a - z). */
-//
+		String s = "aabcccccaaa";
+
+		//Store s in list
+		ArrayList<String> slist = new ArrayList<String>(Arrays.asList(s.split("")));
+		//Create empty list
+		ArrayList<String> elist = new ArrayList<String>();
+		
+		for(int i = 0; i < s.length(); i++) {
+
+			//(s.charAt(i));
+		}
+
+		
 	}
 
 }
