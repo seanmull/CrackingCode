ccccaf927ca9bdc0036b0a73aef78a6cfd8f2a9c
seanmull <kimlan065@gmail.com>
2021-02-09 22:24:24 -0800

    Commit StringRotation

--- CrackingTheCode/src/crackingthecode/StringRotation.java
+++ CrackingTheCode/src/crackingthecode/StringRotation.java
@@ -9,24 +9,26 @@ public class StringRotation {
 		of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one
 		call to isSubstring (e.g., "waterbottle" is a rotation of"erbottlewat"). *///
 		String string = "waterbottle";
-		String stringRotate = "erbottlewat";
-		ArrayList<Integer> indexList = new ArrayList();
-		ArrayList<String> stringRotateList = new ArrayList();
-		char firstChar = stringRotate.charAt(0);
+		String rotatedStringCheck = "erbottlewat";
+		ArrayList<Integer> indexList = new ArrayList<Integer>();
+		ArrayList<String> stringRotateList = new ArrayList<String>();
+		char firstChar = rotatedStringCheck.charAt(0);
 		
-		for (int i = 0; i < string.length(); i++){
+		for (int i = 0; i < string.length(); i++){ //find first char indexes
 			if(string.charAt(i) == firstChar) {
 				indexList.add(i);
 			}
 		}
-		
-		//TODO
-		//Create rotated strings from index found in locations
-			//Break string into two substrings
-			//Join them in reverse order
-		//Compare those strings with stringRotate
-		
-				
+		for (int j = 0; j < indexList.size(); j++) {
+			stringRotateList.add(string.substring(indexList.get(j),string.length()) //right side
+							   + string.substring(0,indexList.get(j))); //left side
+		}
+		for(String rotatedString : stringRotateList) {
+			if(rotatedString.equals(rotatedStringCheck)) {
+				System.out.println(rotatedStringCheck + " is a rotated string of " + string);
+				System.exit(0);			
+			}
+		}
+		System.out.println(rotatedStringCheck + " is not a rotated string of " + string);			
 	}
-
 }
