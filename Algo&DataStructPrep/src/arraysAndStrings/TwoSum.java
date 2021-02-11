3722fb7835a6db21a35c0f48f7c4690ee728ebeb
seanmull <kimlan065@gmail.com>
2021-02-11 13:52:25 -0800

    Added another implementation for TwoSums

--- Algo&DataStructPrep/src/arraysAndStrings/TwoSum.java
+++ Algo&DataStructPrep/src/arraysAndStrings/TwoSum.java
@@ -31,7 +31,7 @@ public class TwoSum {
 		int target = 9;
 		int[] output = new int[2];
 		
-		for(int i = 0; i < nums.length; i++) {
+		/*(for(int i = 0; i < nums.length; i++) {
 			for(int j =0; j < nums.length; j++) {
 				if(j == i) break; //insures not same element
 				if(nums[i] + nums[j] == target) {
@@ -41,11 +41,26 @@ public class TwoSum {
 					System.exit(0);
 				}
 			}
-		}
+		}*/
 		//Runtime is pretty bad with each loop going through each element so O(n^2)
 		//Space usage is constant.
 		
-		//
+		//A better implementation would take into account repeats since both 2 + 7 
+		//and 7 + 2 are the same thing
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = (i+1); j < nums.length; j++) {
+				if(nums[i] + nums[j] == target) {
+					output[0] = i;
+					output[1] = j;
+					System.out.println(Arrays.toString(output));
+					System.exit(0);
+				}
+			}
+		}
+		//The outer loop goes through all number, while the inner loop only through n-1, n-2...1
+		//Which is half of the before example but still 0(n^2)
+		//Space usage is constant.
+		
 
 		
 
