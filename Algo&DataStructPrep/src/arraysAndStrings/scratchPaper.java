c5cc98ddd3df3781c31a5441cb70b5f9f5fa7761
seanmull <kimlan065@gmail.com>
2021-02-11 10:34:07 -0800

    Added to plan

---
+++ Algo&DataStructPrep/src/arraysAndStrings/scratchPaper.java
@@ -0,0 +1,32 @@
+package arraysAndStrings;
+
+public class scratchPaper {
+
+	public static void main(String[] args) {
+		//Strings
+
+		//For making a simple string
+		String s = "abc";
+
+		//To make a char array
+		char[] charArray = s.toCharArray();
+
+		//To index a string
+		//s[0] -> 'a' //note '' means char "" means string
+
+		//Stringbuilder
+
+
+		//Arrays
+
+		//To build a 1-D array
+
+		//To build a 2-D array
+
+		//Typical type implementations
+
+		//
+
+	}
+
+}
