f3f1e40f51673b705bd3e848737a16a6be602b1f
seanmull <kimlan065@gmail.com>
2021-02-12 16:56:16 -0800

    Commit MaxSubArray

--- Algo&DataStructPrep/src/arraysAndStrings/MaxSubArray.java
+++ Algo&DataStructPrep/src/arraysAndStrings/MaxSubArray.java
@@ -1,5 +1,7 @@
 package arraysAndStrings;
 
+import java.util.Arrays;
+
 public class MaxSubArray {
 
 	public static void main(String[] args) {
@@ -30,6 +32,43 @@ public class MaxSubArray {
 
 		Input: nums = [-100000]
 		Output: -100000*/
+
+		//Instance variables
+		int[] nums = new int[] {-100000};
+		int[][] allPossibleSubArrays = new int[nums.length * nums.length][nums.length];
+		int[] sums = new int[nums.length * nums.length];
+		int sum;
+		int maxValue = -999999999;
+		int maxValueIndex = 0;
+		
+		//Get all possible subarrays and add them up
+		for(int subArrayIndex = 0; subArrayIndex < nums.length * nums.length;) { //loops through subarrays
+			for(int start = 0; start < nums.length; start++) { //loops through start points
+				for(int end = 0; end < nums.length; end++) { //loops through end points
+					sum = 0;
+					for(int numIndex = start; numIndex < nums.length - end; numIndex++) { //loops through element in subarray
+						allPossibleSubArrays[subArrayIndex][numIndex] = nums[numIndex]; //for debuging only
+						sum += nums[numIndex];
+					}
+				sums[subArrayIndex] = sum;
+				subArrayIndex++; //go to next subarray after each fill
+				}
+			}
+		}
+		//Compare the sums and get the largest value
+		for(int sumIndex = 0; sumIndex < sums.length; sumIndex++) { 
+			if(sums[sumIndex] > maxValue) { 
+				maxValue = sums[sumIndex]; 
+				maxValueIndex = sumIndex;
+			}
+		}
+		System.out.println("The max value of all subArrays for Array " + Arrays.toString(nums) + " is " + maxValue);
+		System.out.println("The subArray with largest value is " + Arrays.toString(allPossibleSubArrays[maxValueIndex]));
+		
+		//This is O(n^3) in terms of space if we store all the arrays
+		//It is O(n^2) in terms of space if we just collect the sums
+		//Runtime is difficult to quantify, not every value is calculated.
+		//I suspect there is a better implementation since alot of subarrays are zero.
 
 	}
 
