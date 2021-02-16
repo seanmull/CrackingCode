b4d9db290b6be685f71bf25382514a13915ab620
seanmull <kimlan065@gmail.com>
2021-02-15 17:19:13 -0800

    First commit of majority element

--- Algo&DataStructPrep/src/arraysAndStrings/MajorityElement.java
+++ Algo&DataStructPrep/src/arraysAndStrings/MajorityElement.java
@@ -24,6 +24,27 @@ public class MajorityElement {
 		-2^31 <= nums[i] <= 2^31 - 1
 				 
 		Follow-up: Could you solve the problem in linear time and in O(1) space?*/
+		int nums[] = new int[] {2,2,1,1,1,2,2};
+		//First do the brute force solution
+		int count = 0;
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = 0; j < nums.length - 1; j++) {
+				if(nums[i] == nums[j]) count++;
+				if(count > nums.length/2) {
+					System.out.println(nums[i]);
+					break;
+				}
+			}
+		}
+		//Do one loop to track the nums position
+			//Do another loop to track the comparision position
+				//If match increment count
+				//If count greater then n/2
+					//Print current number and break
+		
+		
+		//Then try the follow-up
+
 
 	}
 
