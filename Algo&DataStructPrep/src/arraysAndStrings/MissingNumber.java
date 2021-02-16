ab055b654f644a06d50d0295b62bc6727b6aa19c
seanmull <kimlan065@gmail.com>
2021-02-16 15:13:03 -0800

    Commit missing number

--- Algo&DataStructPrep/src/arraysAndStrings/MissingNumber.java
+++ Algo&DataStructPrep/src/arraysAndStrings/MissingNumber.java
@@ -1,9 +1,12 @@
 package arraysAndStrings;
 
+import java.util.Arrays;
+
 public class MissingNumber {
 
 	public static void main(String[] args) {
-		/*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+		/*Given an array nums containing n distinct numbers in the range [0, n], return the 
+		 *only number in the range that is missing from the array.
 
 		Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
 
@@ -37,7 +40,29 @@ public class MissingNumber {
 		1 <= n <= 10^4
 		0 <= nums[i] <= n
 		All the numbers of nums are unique.*/
-
+		
+		//One solution
+		int[] inputArray = new int[] {9,6,4,2,3,5,7,0,1};
+		//Create a 1-n array with constant space
+		int n = 10000;
+		int[] orderedArray = new int[n+1];
+		boolean isFound;
+		
+		for(int i = 0; i < n+1; i++) orderedArray[i] = i;
+		//Create a loop that iterates this new array
+		for(int i = 0; i < n+1; i++) {
+			isFound = false;
+			for(int j = 0; j < inputArray.length; j++) {
+				if(orderedArray[i] == inputArray[j]) isFound = true;
+			}
+			if(!isFound) {
+				System.out.println("The missing number in Array : " + Arrays.toString(inputArray) + " is " + orderedArray[i]);
+				System.exit(0);
+			}
+		}
+		//The first loop is constant space and runtime since it only goes to 10000
+		//The second loop only goes through n numbers
+		//So space is O(1) and runtime is 1 * n which is O(n)
 	}
 
 }
