55bc6f1d3f278870ff8e008c594c59743452a2a5
seanmull <kimlan065@gmail.com>
2021-02-16 16:47:08 -0800

    Commit containsDuplicate

--- Algo&DataStructPrep/src/arraysAndStrings/ContainsDuplicate.java
+++ Algo&DataStructPrep/src/arraysAndStrings/ContainsDuplicate.java
@@ -19,6 +19,20 @@ public class ContainsDuplicate {
 
 		Input: [1,1,1,3,3,4,3,2,4,2]
 		Output: true*/
+		
+		int nums[] = new int[] {1,1,1,3,3,4,3,2,4,2};
+		
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = i + 1; j < nums.length; j++) {
+				if(nums[i] == nums[j]) {
+					System.out.println("True");
+					System.exit(0);
+				}
+			}
+		}
+		System.out.println("False");
+		
+		//Spaces is O(1) but runtime is n * n - 1 which is O(n^2)
 
 	}
 
