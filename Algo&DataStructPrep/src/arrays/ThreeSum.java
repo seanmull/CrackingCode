29f8e2133719a31f39cffcd995a6be3a76f10e56
seanmull <kimlan065@gmail.com>
2021-02-18 11:43:08 -0800

    Commited threeSum

--- Algo&DataStructPrep/src/arrays/ThreeSum.java
+++ Algo&DataStructPrep/src/arrays/ThreeSum.java
@@ -1,5 +1,6 @@
 package arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ThreeSum {
@@ -29,27 +30,29 @@ public class ThreeSum {
 		0 <= nums.length <= 3000
 		-105 <= nums[i] <= 105*/
 		
-		int nums[] = new int[] {-1,0,1,2,-1,-4};
-		int threeSums[][] = new int[nums.length * nums.length * nums.length][3]; //we also may need an dynamic array so we don't have all these array 0
-		int threeSum[] = new int[3];
-		int threeSumsIndex = 0;
+		int nums[] = new int[] {-1,0,1,2,-1,-4}; //expected output [[-1,-1,2],[-1,0,1]]
+		ArrayList <Integer[]> threeSums = new ArrayList <Integer[]> ();
+		boolean isInThreeSum;
 		
 		for(int aIndex = 0; aIndex < nums.length; aIndex++)
-			for(int bIndex = 0; bIndex < nums.length; bIndex++)
+			for(int bIndex = 0; bIndex < nums.length; bIndex++) {
 				for(int cIndex = 0; cIndex < nums.length; cIndex++) {
-					if(aIndex == bIndex || aIndex == cIndex || bIndex == cIndex) continue; //skip loop if we pull the same number
-					//skip if we have added this three sum already
-					//in other words if all three numbers are int the 
+					isInThreeSum = false;
+					if(aIndex == bIndex || aIndex == cIndex || bIndex == cIndex) continue; 
 					if(nums[aIndex] + nums[bIndex] + nums[cIndex] == 0) {
-						threeSum[0] = nums[aIndex]; threeSum[1] = nums[bIndex]; threeSum[2] = nums[cIndex];
-						threeSums[threeSumsIndex] = threeSum;
-						threeSumsIndex++;
+						Integer threeSum[] = new Integer[] {nums[aIndex], nums[bIndex], nums[cIndex]};
+						Arrays.sort(threeSum);
+						for(Integer[] threeSum1 : threeSums) if(Arrays.equals(threeSum, threeSum1)) isInThreeSum = true;
+						if(isInThreeSum) continue;
+						threeSums.add(threeSum);
 					}
 				}
-		
+			}
 		System.out.println("All unique triplets in the array which gives the sum of zero are: ");
-		for(int[] threeSum1 : threeSums) System.out.println(Arrays.toString(threeSum1));
-
+		for(Integer[] threeSum1 : threeSums) System.out.println("Print whats in threeSums after looping is done " + Arrays.toString(threeSum1));
+		
+		//Runtime is n^3 since we have to look at n * n-1 * n-2 elements in array
+		//Memory Space is more based on combinations of array
 	}
 
 }
