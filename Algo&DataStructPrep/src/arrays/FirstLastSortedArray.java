d3f81bcf443108ada516ef29107bc154f3e774b5
seanmull <kimlan065@gmail.com>
2021-02-19 11:09:07 -0800

    Commit firstLastSorted (do again for log n runtime later)

--- Algo&DataStructPrep/src/arrays/FirstLastSortedArray.java
+++ Algo&DataStructPrep/src/arrays/FirstLastSortedArray.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class FirstLastSortedArray {
 
 	public static void main(String[] args) {
@@ -31,6 +33,30 @@ public class FirstLastSortedArray {
 		-109 <= nums[i] <= 109
 		nums is a non-decreasing array.
 		-109 <= target <= 109*/
+		
+		int nums[] = new int[] {5,7,7,8,8,10};
+		int target = 6;
+		int[] firstLastIndex = new int[2];
+		boolean flag = false;
+		int startIndex = -1;
+		int endIndex = -1;
+		
+		for(int i = 0; i < nums.length - 1; i++) {
+			if(nums[i] == target && !flag) { 
+				startIndex = i;
+				flag = true;
+			}
+			if(nums[i] != nums[i + 1] && nums[i] == target) endIndex = i;
+		}
+		firstLastIndex[0] = startIndex;
+		if(nums[nums.length -1] == target) endIndex = nums.length -1;
+		firstLastIndex[1] = endIndex;
+		
+		System.out.println(Arrays.toString(firstLastIndex));
+		
+		//Runtime is n since we have to look at each element
+		//This is assuming we have no constraints
+		//Space is constant since we only pull a start and end value
 
 	}
 
