7f1bc88a6e0f0a1dd8ccbcbf2026f05f4ad0b7d3
SeansPC <SeansPC@DESKTOP-JQ7P2UF>
2021-02-19 22:24:48 -0800

    Commit RotateImage

--- Algo&DataStructPrep/src/arrays/RotateImage.java
+++ Algo&DataStructPrep/src/arrays/RotateImage.java
@@ -37,21 +37,40 @@ public class RotateImage {
 		1 <= n <= 20
 		-1000 <= matrix[i][j] <= 1000*/
 		
-		int matrix[][] = new int[][] {{1,2},{3,4}};
+		//int matrix[][] = new int[][] {{1,2},{3,4}};
+		int matrix[][] = new int[][] {{1,2,3},{4,5,6},{7,8,9}};
 		int storeNum = matrix[0][0];
 		
-		for(int row = 0; row < matrix.length; row++)
+		/*System.out.println(matrix[0][0]);
+		System.out.println(matrix[0][1]);
+		System.out.println(matrix[1][0]);
+		System.out.println(matrix[1][1]);*/
+		
+		
+		/*storeNum = matrix[0][0];//Store one element
+		matrix[0][0] = matrix[1][0];//Move the left side
+		matrix[1][0] = matrix[1][1];//Move the bottom
+		matrix[1][1] = matrix[0][1];//Move the right side
+		matrix[0][1] = storeNum; //Move the top side*/
+		
+		/*matrix[row][column] = matrix[row + 1][column];//Move the left side still good
+		matrix[row + 1][column] = matrix[row + 1][column + 1];//Move the bottom
+		matrix[row + 1][column + 1] = matrix[row][column + 1];//Move the right side
+		matrix[row][column + 1] = storeNum; //Move the top side*/
+		
+		
+		for(int row = 0; row < 1; row++) {
 			storeNum = matrix[0][0];//Store one element
-			for(int column = 0; column < matrix.length; column++) {
-				
-				matrix[0][1] = storeNum;//Move the top side
-				
-				matrix[1][1] = matrix[0][1];//Move the right side
-				//Move the bottom
-				matrix[0][0] = matrix[1][0];//Move the left side				
+			for(int column = 0; column < 1; column++) {
+				matrix[row][column] = matrix[row + 1][column];//Move the left side still good
+				matrix[row + 2][column] = matrix[row + 2][column + 1];//Move the bottom
+				matrix[row + 1][column + 2] = matrix[row][column + 2];//Move the right side
+				matrix[row][column + 1] = storeNum; //Move the top side*/
 			}
 			
-		System.out.println(Arrays.toString(matrix[0]));
+		for(int[] arrays : matrix) System.out.println(Arrays.toString(arrays));
+		//System.out.println(Arrays.toString(matrix[1]));
+		//System.out.println(Arrays.toString(matrix[2]));
 		
 		//System.out.println(Arrays.toString(matrix[0]));
 				
@@ -63,4 +82,5 @@ public class RotateImage {
 
 	}
 
+}
 }
