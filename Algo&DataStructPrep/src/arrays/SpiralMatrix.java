3ec9939bdc5772dc7e213402092173fd41058794
seanmull <kimlan065@gmail.com>
2021-02-23 09:36:46 -0800

    Commit spiralMatrix

--- Algo&DataStructPrep/src/arrays/SpiralMatrix.java
+++ Algo&DataStructPrep/src/arrays/SpiralMatrix.java
@@ -1,5 +1,6 @@
 package arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SpiralMatrix {
@@ -29,18 +30,44 @@ public class SpiralMatrix {
 		int matrix[][] = new int[][] {{1,2},{3,4}};
 		//int matrix[][] = new int[][] {{1,2,3},{4,5,6},{7,8,9}};//[1,2,3,6,9,8,7,4,5]
 		//int matrix[][] = new int[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12}};//[1,2,3,4,8,12,11,10,9,5,6,7]
-        
-		//if one row/column left, no circle can be formed
+		int m = matrix[0].length; //row length
+		int n = matrix.length; //column length
+		int x = 0; //row position
+		int y = 0; //column position
+		int layer = 0; //initialize layer number
+		ArrayList <Integer> spiralMatrix = new ArrayList <Integer> ();
 		
-		//below, process a circle
-		//top - move right
-		//right - move down
-		//bottom - move left
-		//left - move up
+        while(n > 0 && m > 0) {
+    		//if one row/column left, no circle can be formed
+    		if(m == 1) {
+    			for(int i = 0; i < n; i++) spiralMatrix.add(matrix[x++][y]);
+    			break;
+    		}
+    		if(n == 1) {
+    			for(int i = 0; i < m; i++) spiralMatrix.add(matrix[x][y++]);
+    			break;
+    		}
+    		//below, process a circle
+    		//top - move right
+    		for(int i = 0; i < m - 1; i++) spiralMatrix.add(matrix[x][y++]);
+    		//right - move down
+    		for(int i = 0; i < n - 1; i++) spiralMatrix.add(matrix[x++][y]);
+    		//bottom - move left
+    		for(int i = 0; i < m - 1; i++) spiralMatrix.add(matrix[x][y--]); 
+    		//left - move up
+    		for(int i = 0; i < n - 1; i++) spiralMatrix.add(matrix[x--][y]);
+    		
+    		//move to next layer
+    		y++;
+    		x++;
+    		//decrement rows and columns by two one for each side
+    		m = m - 2;
+    		n = n - 2;
+        }
+
+		System.out.println(spiralMatrix);
 		
-		//increment start position when you go to the next layer
-		//decrement rows and columns by two one for each side
-		//System.out.println(Arrays.toString(spiralMatrix));
+		//Runtime is n*m since it touches each element, memory is also m * n
 		
 		
 
