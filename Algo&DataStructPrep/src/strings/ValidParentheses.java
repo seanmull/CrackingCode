243e3d0c1cc53f324bee728daa4b4dc67631cac2
seanmull <kimlan065@gmail.com>
2021-03-01 10:09:27 -0800

    Commit ValidParenteses

--- Algo&DataStructPrep/src/strings/ValidParentheses.java
+++ Algo&DataStructPrep/src/strings/ValidParentheses.java
@@ -1,5 +1,7 @@
 package strings;
 
+import java.util.ArrayList;
+
 public class ValidParentheses {
 
 	public static void main(String[] args) {
@@ -37,7 +39,54 @@ public class ValidParentheses {
 
 		1 <= s.length <= 104
 		s consists of parentheses only '()[]{}'.*/
+		//String s = "()[]{}";
+		
+		//Doesn't seem simple to convert string to Character[] so skip it for now
+		//Character[] charArray = new Character[] {'(',')', '[',']', '{','}'};
+		//Character[] charArray = new Character[] {'(','[',')',']'};
+		Character[] charArray = new Character[] {'(',']'};
+		ArrayList <Character> bracketList = new  ArrayList <Character>();
+		Character currentEndChar;
+		int currentIndex = 0;
+		
+		//convert to array then list
+		for(int i = 0; i < charArray.length; i++) bracketList.add(charArray[i]);
+		
+		while(bracketList.size() != 0) {
+			currentEndChar = findEndBracket(bracketList.get(currentIndex));
+			if(bracketList.get(currentIndex + 1) == '('  ||  
+			   bracketList.get(currentIndex + 1) == '[' ||
+			   bracketList.get(currentIndex + 1) == '{') {
+				currentIndex++;
+				continue;
+			}
+			if(currentEndChar == bracketList.get(currentIndex + 1)){
+				bracketList.remove(currentIndex);
+				bracketList.remove(currentIndex);
+				currentIndex = 0;
+			}else{
+				System.out.println("false");
+				System.exit(0);
+			}
+		}
+		System.out.println("true");
+	}
+	public static char findEndBracket(char c) {
+		//Would probably be neater to make this into a collection
+		//So you can just map start to end
+		Character[] brackets = new Character[] {'(', ')', '{', '}', '[' ,']'};
+		int bracketIndex = -1;
+		for(int i = 0; i < brackets.length; i++) if(brackets[i] == c) bracketIndex = i;
+		if(bracketIndex == -1 || bracketIndex % 2 != 0) {
+			System.out.println("false");
+			System.exit(0);
+		}else{
+			return brackets[bracketIndex + 1];
+		}
+		return 'e';
 
 	}
+	//Runtime is 0(n) since you need to compare every element memory is also
+	//Linear since you need to copy info into a list
 
 }
