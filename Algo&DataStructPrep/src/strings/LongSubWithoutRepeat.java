8293a49b48ee94ed2919cf77eaa1711a77daf3f3
seanmull <kimlan065@gmail.com>
2021-03-02 11:02:55 -0800

    Commit longsub

--- Algo&DataStructPrep/src/strings/LongSubWithoutRepeat.java
+++ Algo&DataStructPrep/src/strings/LongSubWithoutRepeat.java
@@ -1,12 +1,11 @@
 package strings;
 
+
 public class LongSubWithoutRepeat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/*Given a string s, find the length of the longest substring without repeating characters.
-
-		 
 
 		Example 1:
 
@@ -34,6 +33,49 @@ public class LongSubWithoutRepeat {
 
 		0 <= s.length <= 5 * 104
 		s consists of English letters, digits, symbols and spaces.*/
+		
+		String s = "";
+		char[] str = new char[s.length()];
+		char[] sub = new char[s.length()];
+		int subcount = 1;
+		int max = 0;
+		int subIndex = 1;
+		boolean charFound;
+		
+		str = s.toCharArray();
+		if(s.length() == 0) {
+			System.out.println(0);
+			System.exit(0);
+		}
+		sub[0] = str[0];
+		
+		for(int strIndex = 1; strIndex < s.length();) {
+			charFound = false;
+			for(int findsubIndex = 0; findsubIndex < sub.length; findsubIndex++) {
+				if(str[strIndex] == sub[findsubIndex]) { 
+					sub = new char[s.length()];
+					subIndex = 0;
+					subcount = 0;
+					charFound = true;
+					break;
+				}
+			}
+			if(!charFound) {
+				sub[subIndex] = str[strIndex];
+				subcount++;
+				strIndex++;
+				subIndex++;
+				if(subcount > max) max = subcount;	
+			}
+			
+		}
+		System.out.println(max);
+		
+		//Runtime is n since you need to look at each number memory 
+		//Memory is a bit inefficent since we create new arrays each time
+		//A salution could be to use an array list so I can empty it each
+		//Theres a repeat
+
 	}
 
 }
