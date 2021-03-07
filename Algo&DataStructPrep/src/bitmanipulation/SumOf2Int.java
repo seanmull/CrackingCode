c794fc99d41a2ce5985b92442c401d6ff56dad44
SeansPC <SeansPC@DESKTOP-JQ7P2UF>
2021-03-06 23:03:04 -0800

    Commited sumoftwo

--- Algo&DataStructPrep/src/bitmanipulation/SumOf2Int.java
+++ Algo&DataStructPrep/src/bitmanipulation/SumOf2Int.java
@@ -21,7 +21,63 @@ public class SumOf2Int {
 
 				-1000 <= a, b <= 1000*/
 		
-				//1 + 10 =  11
+				// 1 01  2 10 3 11
+		
+				//11 ^ 1 = 10
+		
+				/*int a = -56; //10
+				int b = 2; //10
+				int c = 0;
+				boolean carryOver = false;
+				int i = 0;
+				
+				
+				while(i != 32) {
+					//Case 1 both are 1
+					if((int)(a & (1 << i)) == (1 << i) && (int) (b & (1 << i)) == (1 << i)) {
+						if(!carryOver) {
+							carryOver = true;
+						}else {
+							c ^= (1 << i);
+						}
+					}
+					
+					//Case 2 one or the other is 1
+					if(((int)(a & (1 << i)) == (1 << i) && (int) (~b & (1 << i)) == (1 << i)) ||
+					   ((int)(~a & (1 << i)) == (1 << i) && (int) (b & (1 << i)) == (1 << i))) {
+						 if(!carryOver) c ^= (1 << i);
+					}
+					
+					//Case 3 both are 0
+					if(((int)(~a & (1 << i)) == (1 << i) && (int) (~b & (1 << i)) == (1 << i))) {
+						 if(carryOver) {
+							 c ^= (1 << i);
+							 carryOver = false;
+						 }
+					}
+					i++;
+				}
+				
+				
+				System.out.println(c);
+				System.out.println(carryOver);*/
+		
+				int a = 2;
+				int b = -3;
+				
+				while(a > 0) {
+					a--;
+					b++;
+				}
+				while(a < 0) {
+					b--;
+					a++;
+				}
+				//System.out.println(b);
+				
+			
+				
+				
 		
 
 				
